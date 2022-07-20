04c4549449abca075f0e6d257ec3e493393f20d2
Stephen Cronin <cronin4392@gmail.com>
2022-07-20 17:46:32 -0400

    More about page content styling and start adding lines

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,3 +1,4 @@
+import React, { useContext } from "react";
 import {
   PageType,
   SectionContentType,
@@ -10,12 +11,22 @@ import styles from "./styles.module.scss";
 
 type PageProps = PageType;
 
+const SectionDepth = React.createContext(-1);
+const SectionIsLast = React.createContext(false);
+
 const Page: React.FC<PageProps> = ({ content }) => {
   return (
-    <div className={styles.Container}>
-      {content.map((section, index) => (
-        <SectionContent {...section} key={index} />
-      ))}
+    <div className={styles.Wrapper}>
+      <div className={styles.Container}>
+        {content.map((section, index) => (
+          <SectionIsLast.Provider
+            value={index === content.length - 1}
+            key={index}
+          >
+            <SectionContent {...section} />
+          </SectionIsLast.Provider>
+        ))}
+      </div>
     </div>
   );
 };
@@ -30,17 +41,70 @@ const SectionContent: React.FC<SectionContentType> = (content) => {
   return null;
 };
 
-const Section: React.FC<SectionType> = ({ title, content }) => (
-  <div className={styles.Section}>
-    <h2 className={styles.SectionTitle}>{title}</h2>
-    {content.map((c, index) => (
-      <SectionContent {...c} key={index} />
-    ))}
-  </div>
-);
+const Section: React.FC<SectionType> = ({ title, content }) => {
+  const depth = useContext(SectionDepth);
 
-const Content: React.FC<ContentType> = ({ content }) => (
-  <div className={styles.Content}>{content}</div>
-);
+  return (
+    <SectionDepth.Provider value={depth + 1}>
+      <div className={styles.Section}>
+        <div>
+          <SectionTitle>{title}</SectionTitle>
+        </div>
+        {content.map((c, index) => (
+          <SectionIsLast.Provider
+            value={index === content.length - 1}
+            key={index}
+          >
+            <SectionContent {...c} />
+          </SectionIsLast.Provider>
+        ))}
+      </div>
+    </SectionDepth.Provider>
+  );
+};
+
+const SectionTitle: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => {
+  const depth = useContext(SectionDepth);
+
+  return (
+    <h2 className={styles.SectionTitle}>
+      <Lines count={depth} />
+      {children}
+    </h2>
+  );
+};
+
+const Content: React.FC<ContentType> = ({ content }) => {
+  const depth = useContext(SectionDepth) + 1;
+
+  return (
+    <div className={styles.Content}>
+      <div className={styles.ContentLines}>
+        <Lines count={depth} />
+      </div>
+      <div>
+        {content}
+        <br />
+      </div>
+    </div>
+  );
+};
+
+const Lines: React.FC<{ count: number }> = ({ count }) => {
+  const mapCount = [...Array(count)];
+  const isLast = useContext(SectionIsLast);
+  const depth = useContext(SectionDepth);
+
+  return (
+    <span className={styles.Lines} data-depth={depth} data-is-last={isLast}>
+      {mapCount.map((_, index) => (
+        <span className={styles.LineStart} key={index} />
+      ))}
+      <span className={styles.LineEnd} />
+    </span>
+  );
+};
 
 export default Page;
