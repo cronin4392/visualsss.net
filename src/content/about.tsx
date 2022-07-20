da6550ad8de387820f68928fa7b0f9992afc1d6f
Stephen Cronin <cronin4392@gmail.com>
2022-07-20 19:47:04 -0400

    Add some more about page content and style links

--- src/content/about.tsx
+++ src/content/about.tsx
@@ -1,3 +1,4 @@
+import LinkWrapper from "@/components/LinkWrapper";
 import { PageType } from "@/types/page";
 
 export const aboutPageContent: PageType["content"] = [
@@ -10,14 +11,20 @@ export const aboutPageContent: PageType["content"] = [
         content: (
           <>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed
-              facilisis ipsum. Fusce in risus sit amet justo fringilla iaculis.
-              Phasellus tincidunt erat sit amet mi blandit tempor. Donec ut
-              hendrerit orci, sed egestas elit. Aenean fermentum dignissim mi,
-              et gravida nisl accumsan id. Suspendisse porta nisi non ante
-              iaculis posuere. Curabitur pretium turpis id facilisis lacinia.
-              Donec quis gravida mauris. Aliquam scelerisque urna vel metus
-              consequat euismod. Suspendisse aliquam ullamcorper ultricies.
+              I am a{" "}
+              <LinkWrapper href="https://en.wikipedia.org/wiki/VJing">
+                VJ
+              </LinkWrapper>{" "}
+              that goes by the name visualsssssssss. My interest in VJing
+              started back in 2017 after being inspired while attending
+              electronic music shows. I shortly thereafter discovered the
+              real-time software{" "}
+              <LinkWrapper href="https://derivative.ca/">
+                Touchdesigner
+              </LinkWrapper>{" "}
+              and became enthralled in it. Using my experience in software
+              development and graphic design I began to make original content in
+              it centered around typography.
             </p>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed
@@ -42,7 +49,11 @@ export const aboutPageContent: PageType["content"] = [
         type: "content",
         content: (
           <>
-            <p>This site is built with Next.js yada yada yada</p>
+            <p>
+              This site is built with{" "}
+              <LinkWrapper href="https://nextjs.org/">Next.js</LinkWrapper> yada
+              yada yada
+            </p>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed
               facilisis ipsum. Fusce in risus sit amet justo fringilla iaculis.
