f24b936193df42f2a76490d9c364635b416e9273
Stephen Cronin <cronin4392@gmail.com>
2022-09-27 00:13:57 -0400

    Simplify about page and remove crazy structure

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,111 +1,13 @@
-import React, { useContext } from "react";
-import { useElementSize } from "@mantine/hooks";
-import {
-  PageType,
-  SectionContentType,
-  SectionType,
-  ContentType,
-  isSection,
-  isContent,
-} from "@/types/page";
-import { getFontSize, getLineCount } from "@/utils/text";
+import React from "react";
 import styles from "./styles.module.scss";
-
-type PageProps = PageType;
 
 const SectionDepth = React.createContext(-1);
 
-const Page: React.FC<PageProps> = ({ content }) => {
+const Page: React.FC<{ content: React.ReactNode }> = ({ content }) => {
   return (
     <div className={styles.Wrapper}>
-      <div className={styles.Container}>
-        {content.map((section, index) => (
-          <SectionContent {...section} key={index} />
-        ))}
-      </div>
+      <div className={styles.Container}>{content}</div>
     </div>
-  );
-};
-
-const SectionContent: React.FC<SectionContentType> = (props) => {
-  const { ...content } = props;
-
-  if (isSection(content)) {
-    return <Section {...content} />;
-  }
-  if (isContent(content)) {
-    return <Content {...content} />;
-  }
-  return null;
-};
-
-const Section: React.FC<SectionType> = ({ title, content }) => {
-  const depth = useContext(SectionDepth);
-
-  return (
-    <SectionDepth.Provider value={depth + 1}>
-      <>
-        <SectionTitle>{title}</SectionTitle>
-        {content.map((c, index) => (
-          <SectionContent {...c} key={index} />
-        ))}
-      </>
-    </SectionDepth.Provider>
-  );
-};
-
-const SectionTitle: React.FC<{
-  children: React.ReactNode;
-}> = ({ children }) => {
-  const depth = useContext(SectionDepth);
-
-  return (
-    <h2 className={styles.SectionTitle}>
-      <Lines count={depth} />
-      {children}
-    </h2>
-  );
-};
-
-const Content: React.FC<ContentType> = ({ content }) => {
-  const { ref, height } = useElementSize();
-  const depth = useContext(SectionDepth) + 1;
-  const fontSize = getFontSize(ref);
-  const lines = getLineCount(height, fontSize);
-
-  return (
-    <div className={styles.Content}>
-      <div className={styles.ContentLines}>
-        <Lines count={depth} />
-        {lines > 0 && (
-          <>
-            {[...Array(lines)].map((_, index) => (
-              <Lines count={depth} key={index} />
-            ))}
-            <Lines count={depth - 1} />
-          </>
-        )}
-      </div>
-      <div>
-        <br />
-        <div ref={ref}>{content}</div>
-      </div>
-    </div>
-  );
-};
-
-const Lines: React.FC<{
-  count: number;
-}> = ({ count }) => {
-  const mapCount = [...Array(count)];
-
-  return (
-    <span className={styles.Lines}>
-      {mapCount.map((_, index) => (
-        <span className={styles.LineStart} key={index} />
-      ))}
-      <span className={styles.LineEnd} />
-    </span>
   );
 };
 
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,7 +3,7 @@ import PageLayout from "@/layouts/PageLayout";
 import HeadTag from "@/components/HeadTag";
 import Header from "@/components/Header";
 import Page from "@/components/Page";
-import { aboutPageContent } from "@/content/about";
+import LinkWrapper from "@/components/LinkWrapper";
 
 const AboutPage: NextPage = () => {
   return (
@@ -11,7 +11,36 @@ const AboutPage: NextPage = () => {
       <HeadTag />
       <PageLayout
         header={<Header subLine="About" />}
-        content={<Page content={aboutPageContent} />}
+        content={
+          <Page
+            content={
+              <p>
+                I am a{" "}
+                <LinkWrapper href="https://en.wikipedia.org/wiki/VJing">
+                  VJ
+                </LinkWrapper>{" "}
+                that goes by the name visualsss. My interest in VJing started
+                back in 2017 after attending a music festival. I had no idea
+                VJing even existed and I was blown away by what I saw. I shortly
+                thereafter discovered the real-time software{" "}
+                <LinkWrapper href="https://derivative.ca/">
+                  Touchdesigner
+                </LinkWrapper>{" "}
+                and became enthralled in it. Using my experience in software
+                development and graphic design I began to make original content
+                in it centered around typography. Check out my{" "}
+                <LinkWrapper href="https://www.youtube.com/c/Visualsss">
+                  Youtube
+                </LinkWrapper>{" "}
+                to see some tutorials I&rsquo;ve made for Touchdesigner and see
+                some of my work on my{" "}
+                <LinkWrapper href="https://www.instagram.com/visualsssssssss/">
+                  Instagram
+                </LinkWrapper>
+              </p>
+            }
+          />
+        }
       />
     </>
   );
