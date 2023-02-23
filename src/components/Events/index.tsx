d5ac1e39f367d7febadcd0eefcbee96cef07ab92
Stephen Cronin <stephen.cronin@tulip.co>
2023-02-22 19:49:26 -0500

    Add dimensions to event image

--- src/components/Events/index.tsx
+++ src/components/Events/index.tsx
@@ -51,8 +51,8 @@ const Event: React.FC<Event> = ({ file, url }) => {
           src={file}
           alt="Live generated projection"
           layout="responsive"
-          width="100%"
-          height="100%"
+          width={640}
+          height={640}
         />
       </LinkWrapper>
     </div>
