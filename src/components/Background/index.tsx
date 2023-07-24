31ea17459ad32f0d26e6e0ccbd309b679ce8c4a8
Stephen Cronin <stephen.cronin@tulip.co>
2023-07-24 00:44:40 -0400

    Fade Background video on non video pages

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
 import { scale } from "@/utils/math";
 import { useVideo } from "@/context/VideoIndexContextProvider";
@@ -11,29 +12,33 @@ const ReactHlsPlayer = dynamic(() => import("react-hls-player"), {
 type BackgroundProps = {};
 
 const Background: React.FC<BackgroundProps> = () => {
+  const router = useRouter();
   const { video } = useVideo();
   const playerRef = useRef<HTMLVideoElement | null>(null);
+  const videoFaded = !["/video"].includes(router.asPath);
 
   return (
     <div className={styles.Container}>
-      <ReactHlsPlayer
-        className={styles.Video}
-        playerRef={playerRef}
-        src={video.url}
-        autoPlay={true}
-        controls={false}
-        muted={true}
-        loop={true}
-        playsInline={true}
-        onLoadedMetadata={(_event) => {
-          if (playerRef.current) {
-            const video = playerRef.current;
-            const { duration } = video;
-            const randomTime = scale(Math.random(), 0, 1, 0, duration);
-            video.currentTime = randomTime;
-          }
-        }}
-      />
+      <div className={styles.VideoContainer} data-video-faded={videoFaded}>
+        <ReactHlsPlayer
+          className={styles.Video}
+          playerRef={playerRef}
+          src={video.url}
+          autoPlay={true}
+          controls={false}
+          muted={true}
+          loop={true}
+          playsInline={true}
+          onLoadedMetadata={(_event) => {
+            if (playerRef.current) {
+              const video = playerRef.current;
+              const { duration } = video;
+              const randomTime = scale(Math.random(), 0, 1, 0, duration);
+              video.currentTime = randomTime;
+            }
+          }}
+        />
+      </div>
       <div className={styles.LoadingContainer}>
         <div className={styles.Loading}></div>
       </div>
